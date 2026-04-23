6b45306c0cded16b0fccbd16b856210f3c95bd21
Punnoose Punnen <punnoose.punnen@goniyo.com>
2026-04-23 12:59:40 +0530

    rate limiting

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -6,6 +6,10 @@ import { ConfigModule } from '@nestjs/config';
 import { ProxyModule } from './proxy/proxy.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthMiddleware } from './auth.middleware';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
+import Redis from 'ioredis'
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,9 +18,20 @@ import { AuthMiddleware } from './auth.middleware';
     ConfigModule.forRoot(
       { isGlobal: true }
     ),
+    ThrottlerModule.forRoot({
+      throttlers: [{ ttl: 60000, limit: 100 }],
+      storage: new ThrottlerStorageRedisService(
+        new Redis(process.env.REDIS_URL ?? 'redis://localhost:6379')
+      ),
+    }),
   ],
   controllers: [AppController, AuthController],
-  providers: [AppService, AuthMiddleware],
+  providers: [AuthMiddleware,
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
